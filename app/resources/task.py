b5e30c9fb9f6a182e822dd758b21929600a97bc3
Robert Jones <rj694@bath.ac.uk>
2025-06-04 13:05:00 +0100

    Fix NameError by importing 'joinedload' from SQLAlchemy ORM

--- app/resources/task.py
+++ app/resources/task.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from marshmallow import ValidationError
 from datetime import datetime, timedelta
 from sqlalchemy import desc, asc
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import selectinload, joinedload
 from app import db
 from app.models.task import Task
 from app.models.tag import Tag
